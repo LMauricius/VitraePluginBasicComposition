660c8ece30195d7b778bc0fe9225da7f7870b283
LMauricius <mauriciosmit100@gmail.com>
2025-03-29 18:36:05 +0100

    All needed setup files

--- src/Setup.cpp
+++ src/Setup.cpp
@@ -1,8 +1,24 @@
 #include "VitraePluginBasicComposition/Setup.hpp"
 
+#include "VitraePluginBasicComposition/tasks/displayCamera.hpp"
+#include "VitraePluginBasicComposition/tasks/displayNormals.hpp"
+#include "VitraePluginBasicComposition/tasks/helpers.hpp"
+#include "VitraePluginBasicComposition/tasks/renderForward.hpp"
+#include "VitraePluginBasicComposition/tasks/renderSilhouette.hpp"
+#include "VitraePluginBasicComposition/tasks/shadingTransform.hpp"
+#include "VitraePluginBasicComposition/tasks/shadowCommon.hpp"
+
 namespace VitraePluginBasicComposition
 {
 
-void setup(Vitrae::ComponentRoot &root) {}
+void setup(Vitrae::ComponentRoot &root)
+{
+    setupDisplayCamera(root);
+    setupDisplayNormals(root);
+    setupRenderForward(root);
+    setupRenderSilhouette(root);
+    setupShadingTransform(root);
+    setupShadowCommon(root);
+}
 
 } // namespace VitraePluginBasicComposition
